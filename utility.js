76e3be37b88d2c81f3dddb87a988e066d67639e9
TheButtskie <65842772+TheButtskie@users.noreply.github.com>
2021-11-12 00:40:42 -0500

    Create utility.js

---
+++ utility.js
@@ -0,0 +1,34 @@
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+getJSON('https://fastapi-btest.herokuapp.com/',
+function(err, data) {
+  if (err !== null) {
+    console.log('Something went wrong: ' + err);
+  } else {
+    console.log('Your query count: ' + data.data);
+  }
+});
+
+
+// cycle through the images
+var getImgs = function(data) {
+  var image_location = document.getElementsByTagName("img")[0];
+  for (let i = 0; i < data.length; i++) {
+    setTimeout(function(){
+  	  image_location.setAttribute("src", data[i][0]);
+  	}, 2000*i);
+  }
+};
